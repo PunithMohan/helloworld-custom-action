4aca59e689f0c3df656d8749eed87c15a5a10256
PunithMohan <87635208+PunithMohan@users.noreply.github.com>
2021-11-26 20:33:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Hello, world!');
+// console.log('Hello, world!');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -6,6 +6,15 @@ try {
   // `who-to-greet` input defined in action metadata file
   const commitCount = core.getInput('commit-count');
   console.log(`Commit Count ${commitCount}!`);
+  
+  //get input label name
+  const LabelName1 = core.getInput('LabelName');
+  console.log(`LabelName ${LabelName1}!`);
+  
+  //get input label name
+  const Comments1 = core.getInput('Comments');
+  console.log(`Comments1 ${Comments1}!`);
+  
   var CommitsCounts = commitCount;
   core.setOutput("CommitsCounts", CommitsCounts);
   console.log(`Commits Count ${CommitsCounts}!`)
