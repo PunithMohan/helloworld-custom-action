4e5803177436aaec609d3887fe887126114909e2
PunithMohan <87635208+PunithMohan@users.noreply.github.com>
2021-11-27 14:09:24 +0530

    async

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ try {
   var comments = core.getInput('Comments');
   //core.setOutput("comments", comments);
   console.log(`comments ${comments}`);
-  
+  /*
  const url = "https://api.github.com/repos/PunithMohan/helloworld-action/pulls/1/commits";
  console.log(`url ${url}`);
  const options = {
@@ -27,9 +27,34 @@ try {
   }
 };
 
+
 fetch(url, options)
   .then( res => res.json() )
   .then( data => console.log(data) );
+  
+ */
+  
+  async function postData(url = '' , data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: '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', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+  postData('https://api.github.com/repos/PunithMohan/helloworld-action/pulls/1/commits', {})
+  .then(data => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
